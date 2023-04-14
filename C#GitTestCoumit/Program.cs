75e2b680c7f2f44fe1eada5df4fc8181f37dfb43
SeongWon Jeon <gateoper1105@gmail.com>
2023-04-14 20:14:37 +0900

    feat : 테스트 문제 2번

--- C#GitTestCoumit/Program.cs
+++ C#GitTestCoumit/Program.cs
@@ -14,7 +14,21 @@
                 searchText = "-1";
             }
             Console.WriteLine(Text2.IndexOf(searchText));
-            
+
+            // 2. 문자열을 입력받으면 단어의 개수를 출력하기
+            int A = 1;
+
+            string str = Console.ReadLine();
+
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    A++;
+                }
+            }
+            Console.WriteLine(A);
         }
     }
 }
