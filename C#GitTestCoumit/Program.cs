74e8dbc8fc891314a99119f20953fc5bde892dab
SeongWon Jeon <gateoper1105@gmail.com>
2023-04-14 20:16:38 +0900

    feat : 테스트 문제 3번

--- C#GitTestCoumit/Program.cs
+++ C#GitTestCoumit/Program.cs
@@ -29,6 +29,53 @@
                 }
             }
             Console.WriteLine(A);
+
+            // 3. 주어진 숫자가 소수인지 판별하는 solution을 완성하라
+
+            static bool IsPrime(int n)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                bool minority = true;
+
+
+                if (number == 2)
+                {
+                    minority = true;
+                }
+                else if (number % 2 == 0)
+                {
+                    minority = false;
+                }
+                else
+                {
+                    for (int i = 3; i < number; i++)
+                    {
+                        if (i % number == 0)
+                        {
+                            minority = false;
+                            break;
+                        }
+                        else minority = true;
+                    }
+                }
+                if (minority = true)
+                {
+                    Console.WriteLine("소수입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("소수가아닙니다.");
+                }
+                return minority;
+            }
+
+            // 4.
+
+
+
+
+
         }
     }
 }
